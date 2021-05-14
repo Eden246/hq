3a8ced5d094936b4b54c9e71f275d8526a87ee3c
Eden246 <65497652+Eden246@users.noreply.github.com>
2021-05-14 23:57:59 +0900

    Update settings.py

--- hq/settings.py
+++ hq/settings.py
@@ -143,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jofew7@gmail.com'
-EMAIL_HOST_PASSWORD = 'B_3fNSrsB'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'ライフテクノサービス<jofew7@gmail.com>'
+DEFAULT_FROM_EMAIL = '>'
