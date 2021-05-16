1fdceacec0792803e73d45d0eff8cc139d034503
Eden246 <jofew@naver.com>
2021-05-17 08:54:32 +0900

    5/17

--- customer/views.py
+++ customer/views.py
@@ -182,7 +182,7 @@ class CartView(View):
         send_mail(
             f'{facility}の{name}様、ライフテクノサービス（津営業所）予約完了メール',
             body,
-            'so-kan@life-techno.jp',
+            'ライフテクノサービス・レンタル事業部<so-kan@life-techno.jp>',
             [email],
             fail_silently= False
         )
