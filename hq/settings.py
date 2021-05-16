a6e19ca7318924cfbff08fe3885ca707f41c6186
Eden246 <jofew@naver.com>
2021-05-17 08:43:24 +0900

    5/17

--- hq/settings.py
+++ hq/settings.py
@@ -143,8 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'jofew7@gmail.com'
+EMAIL_HOST_PASSWORD = 'B_3fNSrsB'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = ''
