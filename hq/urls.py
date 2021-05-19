a841fd88ef40e2134df3410785a4b462d398713a
Eden246 <jofew@naver.com>
2021-05-19 17:35:25 +0900

    5/19

--- hq/urls.py
+++ hq/urls.py
@@ -59,9 +59,7 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
     path('order/', include('customer.urls')),
     path('dashboard/', include('dashboard.urls')),
+
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
