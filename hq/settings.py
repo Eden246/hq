44dddd75b634e82f5295c04d08a34e0047baf02a
Eden246 <jofew@naver.com>
2021-05-31 09:08:23 +0900

    heroku

--- hq/settings.py
+++ hq/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'hq.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'postgres',
+        'USER':'postgres',
+        'PASSWORD':'eijows7sm',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 # Password validation
